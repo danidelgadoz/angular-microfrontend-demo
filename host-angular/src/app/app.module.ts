6edd20bbd7f210bad29e7975b3de5eedc73aead5
Dandel <dedd1993@gmail.com>
2021-07-26 01:10:25 -0500

    feat(host-angular): implement weather microfront in angular host

--- host-angular/src/app/app.module.ts
+++ host-angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
